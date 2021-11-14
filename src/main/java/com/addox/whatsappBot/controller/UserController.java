5af25f1abcda8e1203e08f949919e75bb571d750
nazaltk <nazal.tk@gmail.com>
2021-11-14 14:04:24 +0530

    AddedScreenshot

--- src/main/java/com/addox/whatsappBot/controller/UserController.java
+++ src/main/java/com/addox/whatsappBot/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.addox.whatsappBot.entity.User;
@@ -15,9 +16,14 @@ public class UserController {
 
 	@Autowired
 	UserService userService;
-	
+
 	@RequestMapping(method = RequestMethod.POST, value = "/create")
 	public User createUser(@RequestBody User userDetails) {
 		return userService.createUser(userDetails);
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/screenshot")
+	public byte[] createUser(@RequestParam String userId) {
+		return userService.getScreenshot(Long.parseLong(userId));
 	}
 }
